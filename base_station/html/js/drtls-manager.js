d4a3f8ca91f50721a056258e6dae087419683b57
Souleymane <souleymane11dembele@gmail.com>
2024-05-17 08:40:32 -0700

    Update drtls-manager.js

--- base_station/html/js/drtls-manager.js
+++ base_station/html/js/drtls-manager.js
@@ -10,32 +10,32 @@
  *
  */
 
-const boundary = {
-    xMin: 0, xMax: 100,
-    yMin: 0, yMax: 100,
-    zMin: 0, zMax: 100
-};
-
 function isTagWithinBoundary(position) {
+    const boundary = {
+        xMin: 0, xMax: 100,
+        yMin: 0, yMax: 100,
+        zMin: 0, zMax: 100
+    };
+
     return position.x >= boundary.xMin && position.x <= boundary.xMax &&
            position.y >= boundary.yMin && position.y <= boundary.yMax &&
            position.z >= boundary.zMin && position.z <= boundary.zMax;
 }
 
 function drawBoundary() {
+    const geometry = new THREE.Geometry();
     const material = new THREE.LineBasicMaterial({ color: 0xff0000 }); // color red
-    const points = [
-        new THREE.Vector3(boundary.xMin, boundary.yMin, boundary.zMin),
-        new THREE.Vector3(boundary.xMax, boundary.yMin, boundary.zMin),
-        new THREE.Vector3(boundary.xMax, boundary.yMax, boundary.zMin),
-        new THREE.Vector3(boundary.xMin, boundary.yMax, boundary.zMin),
-        new THREE.Vector3(boundary.xMin, boundary.yMin, boundary.zMin)
-    ];
 
-    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    geometry.vertices.push(new THREE.Vector3(boundary.xMin, boundary.yMin, boundary.zMin));
+    geometry.vertices.push(new THREE.Vector3(boundary.xMax, boundary.yMin, boundary.zMin));
+    geometry.vertices.push(new THREE.Vector3(boundary.xMax, boundary.yMax, boundary.zMin));
+    geometry.vertices.push(new THREE.Vector3(boundary.xMin, boundary.yMax, boundary.zMin));
+    geometry.vertices.push(new THREE.Vector3(boundary.xMin, boundary.yMin, boundary.zMin)); 
+
     const line = new THREE.Line(geometry, material);
     scene.add(line);
 }
+
 
 function sendMessage(tagId, position) {
     const accountSid = "AC595211c5deab5bd9d19e0c1764cb4c7b";
@@ -393,6 +393,7 @@ var renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sceneWidth, sceneHeight);
 renderer.setClearColor(0xffffff);
+document.getElementById('scene').appendChild(renderer.domElement);
 
 scene.add(new THREE.AmbientLight(0xf0f0f0));
 
